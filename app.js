e38b005f0b9f512c14d7f26cad26086f268919bf
Victor Kuo <vck3000@gmail.com>
2018-12-28 23:02:33 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+var path = require('path');
+
+app.use(express.static('assets'))
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+
